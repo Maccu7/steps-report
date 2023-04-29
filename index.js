21c559a9f2173bd66704f68d459232963feb6275
Your name <you@example.com>
2023-04-29 12:23:15 +0200

    new challenge

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function number1() {
+    document.getElementById("form").style.display= "block";
+    document.getElementById("step-2").style.display = "none";
+    document.getElementById("thank-you").style.display = "none";
+}
+function number2() {
+    document.getElementById("step-2").style.display = "block";
+    document.getElementById("form").style.display= "none";
+    
+  }
+  function number2() {
+    document.getElementById("step-2").style.display = "block";
+    document.getElementById("form").style.display= "none";
+    document.getElementById("thank-you").style.display = "none";
+  }
+  function number4() {
+    document.getElementById("thank-you").style.display = "block";
+    document.getElementById("form").style.display= "none";
+    document.getElementById("step-2").style.display = "none";
+    
+  }
+  
+   
