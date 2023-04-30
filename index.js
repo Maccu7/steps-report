1739d1b05ced806510315bfe48b0f6508d8ce92c
Your name <you@example.com>
2023-04-30 22:45:14 +0200

    new changes

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ function number1() {
     document.getElementById("step-2").style.display = "none";
     document.getElementById("thank-you").style.display = "none";
 }
-function number2() {
-    document.getElementById("step-2").style.display = "block";
-    document.getElementById("form").style.display= "none";
-    
-  }
   function number2() {
     document.getElementById("step-2").style.display = "block";
     document.getElementById("form").style.display= "none";
